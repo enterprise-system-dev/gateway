ea1ec293150f21610badbe715c29ae0e2efd907d
education-bootcamp <education.seekerscloud@gmail.com>
2024-11-16 12:42:29 +0530

    jhg

--- src/main/java/config/GatewayConfig.java
+++ src/main/java/config/GatewayConfig.java
@@ -1,22 +1,21 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 @Configuration
 public class GatewayConfig {
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 
         return builder.routes()
-                .route("user-service-api", r->r.path("/user-service/**")
+                .route("user-service-api", r -> r.path("/user-service/**")
                         .uri("http://localhost:8083"))
-                .route("product-api", r->r.path("/products-service/**")
-                        .uri("http://localhost:8081"))
-                .route("order-payment-service", r->r.path("/order-payment-service/**")
-                        .uri("http://localhost:3000")).build();
-
+                .build();
     }
 }
--- src/main/java/security/SecurityConfiguration.java
+++ src/main/java/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -29,15 +30,10 @@ public class SecurityConfiguration {
                 .authorizeExchange(authorize ->
                         authorize
                                 .pathMatchers("user-service/api/v1/**").permitAll()
-                                .pathMatchers("products-service/api/v1/**").permitAll()
-                                .pathMatchers("order-payment-service/api/v1/**").permitAll()
-                                .pathMatchers("/eureka/**")
-                                .permitAll()
                                 .anyExchange()
                                 .authenticated())
                 /*  .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));*/
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-
 
         return  serverHttpSecurity.build();
     }
@@ -45,14 +41,14 @@ public class SecurityConfiguration {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(List.of("*")); // Specify your allowed origin
+        corsConfig.setAllowedOrigins(List.of("*"));
         corsConfig.setAllowedMethods(List.of("*"));
         corsConfig.setMaxAge(3600L);
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(false); // Allow credentials
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source =
+                new org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
